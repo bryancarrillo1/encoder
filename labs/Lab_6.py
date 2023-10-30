d8d2eec50f7db29b58e18ff0d0e238cfaae22ef0
Brandosp <brandosp05@gmail.com>
2023-10-30 12:08:57 -0400

    test

--- labs/Lab_6.py
+++ labs/Lab_6.py
@@ -1,5 +1,8 @@
-def encoder():
-    password = input("Enter a password to encode: ")
+# Bryan Carrillo & Brando Santana
+
+
+def encoder(password):
+
     string = ""
 
     for i in password:
@@ -16,8 +19,33 @@ def encoder():
 
         string += str(encoded)
 
-    return print(f'Your encoded password is {string}')
+    return string
 
+def decoder(encodedPassword):
+
+    string = ""
+
+    for i in encodedPassword:
+        temp = int(i)
+
+        if i == "2":
+            num = 9
+            new = str(num)
+            string += new
+        elif i == "1":
+            num = 8
+            new = str(num)
+            string += new
+        elif i == "0":
+            num = 7
+            new = str(num)
+            string += new
+        else:
+            num = temp - 3
+            new = str(num)
+            string += new
+
+    return string
 
 def main():
     run = True
@@ -31,21 +59,24 @@ def main():
 
         option = int(input("Please enter an option: "))
 
-        if option == 1:
-            encoder()
 
-        # if option == 2:
-        #     decoder()
+
+        if option == 1:
+            password = input("Enter a password to encode: ")
+
+            encodedPassword = encoder(password)
+
+            print("Your password has been encoded and stored!")
+
+        if option == 2:
+            decodedPassword = decoder(encodedPassword)
+
+            print(f'The encoded password is {encodedPassword}, and the original password is {decodedPassword}.')
 
         if option == 3:
             run = False
 
 
-def tsee():
-    pass
-
-def test():
-    pass
 
 if __name__ == '__main__':
     main()
